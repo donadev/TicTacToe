b9abf4df16b81096e9d00172b1b3a94a73bf6d44
Luigi Donadel <luigi.donadel@gmail.com>
2022-01-30 14:36:33 +0100

    fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export const MAIN_README_ASSET_PATH = "./assets/main_readme_template.txt"
 export const MOVES_FILE_NAME = "moves.txt"
 export const MATRIX_FILE_NAME = "matrix.txt"
 export const GAME_NAME_FILE_NAME = "name.txt"
-export const README_FILE_NAME = "Readme.md"
+export const README_FILE_NAME = "README.md"
 export const OUTPUT_IMAGE_NAME = "output.png"
 
 export const README_PATH = `./${README_FILE_NAME}`
